0191e23791dabc9d2299bf5f4cf135d9f7586dcb
Oliver Eielson <47375940+olivereielson@users.noreply.github.com>
2022-10-26 18:30:44 -0400

    Test

--- json_stuff.js
+++ json_stuff.js
@@ -1,52 +1,88 @@
 //json data
 let data;
+let genre = [];
 
 //function that searches through the JSON for selected Val
-function selection(){
+function selection() {
     //get the val from the doc.
-    let val=document.getElementById("selector").value;
+    let val = document.getElementById("selector").value;
+    //fix symbole issues
+    val= val.replaceAll("&","&amp;");
+
     //if the vall is all show the entire table
-    if(val==="All"){
+    if (val === "All") {
         makeTable(data.song);
 
-    }else{
-        //else show the artist
-        let select= data.song.find(element => element.Artist===val);
-        makeTable([select])
+    } else {
+        //else show the genre
+        let select = [];
+        for (let x in data.song) {
+            console.log(val);
+            console.log(data.song[x]);
+            if (data.song[x].Genre.includes(val)) {
+                //store the JSON as a list of strings.... I am sure there is a better way to do this but it works and
+                // I have to much work to make a more elegant solution. 
+                select.push(JSON.stringify(data.song[x]));
+            }
+        }
+        makeSelectTable(select)
     }
-  
-} 
 
-//Function to make a table
-function makeTable(list){
+}
+
+
+//make table with filtered data
+function makeSelectTable(list) {
     let text = "<table>"
     //add the column headers
     text += "<tr><td>Song Title</td><td>Artist</td><td>Release Date</td><td>Genre</td></tr>";
-    //add the column vals
     for (let x in list) {
-        text += "<tr><td>" + list[x].title + "</td><td>" + list[x].Artist + "</td><td>" + list[x].Released + "</td><td>" + list[x].Genre.toString() + "</td></tr>";
+        //parse the JSON
+        let temp = JSON.parse(list[x]);
+        text += "<tr><td>" + temp.title + "</td><td>" + temp.Artist + "</td><td>" + temp.Released + "</td><td>" + temp.Genre + "</td></tr>";
     }
     text += "</table>"
     //add table to doc
     document.getElementById("table").innerHTML = text;
 }
+
+//Function to make a table
+function makeTable(list) {
+    let text = "<table>"
+    //add the column headers
+    text += "<tr><td>Song Title</td><td>Artist</td><td>Release Date</td><td>Genre</td></tr>";
+    //add the column vals
+    for (let x in list) {
+        text += "<tr><td>" + list[x].title + "</td><td>" + list[x].Artist + "</td><td>" + list[x].Released + "</td><td>" + "list[x].Genre" + "</td></tr>";
+    }
+    text += "</table>"
+    //add table to doc
+    document.getElementById("table").innerHTML = text;
+}
+
 //function to make selectors
-function makeSelector(list){
-    
+function makeSelector(list) {
+
     let text_select = "<select id='selector' onchange='selection()'>"
     //add the 'All' option
     text_select += "<option value='All'>All</option>";
-    //add artists
+    //add genres
     for (let x in list) {
-        text_select += "<option value='"+list[x].Artist+"'>" + list[x].Artist+ "</option>";
+        for (let y in list[x].Genre) {
+
+            if (!genre.includes(list[x].Genre[y])) {
+                genre.push(list[x].Genre[y]);
+                text_select += "<option value='" + list[x].Genre[y] + "'>" + list[x].Genre[y] + "</option>";
+            }
+
+
+        }
     }
 
     text_select += "</select>"
     //add to doc
     document.getElementById("table2").innerHTML = text_select;
 }
-
-
 
 
 //fetch the json text
@@ -63,7 +99,7 @@ fetch('./songs.json')
         let list = data.song;
         makeSelector(list);
         makeTable(list);
-       
+
 
     });
 
